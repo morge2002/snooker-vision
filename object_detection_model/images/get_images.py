779acadf2990939e5450979fd50af2f0e5ef5616
Morgan Lewis <morganlewis2002@gmail.com>
2023-01-28 17:10:40 +0000

    Pull, store and load image dataset

--- object_detection_model/images/get_images.py
+++ object_detection_model/images/get_images.py
@@ -5,25 +5,23 @@ Test object detection for snooker/pool table and balls with Google's open images
 import fiftyone as fo
 
 
-# import torch
-
-# train_dataset = fo.zoo.load_zoo_dataset(
-#     "open-images-v6",
-#     split="train",
-#     label_types=["detections"],
-#     classes=["Billiard table"],
-# )
-# print("dataset loaded. Exporting dataset...")
-# train_dataset.export(
-#     export_dir=os.path.join(os.path.dirname(__file__), "datasets/snooker_vision/train"),
-#     dataset_type=fo.types.YOLOv5Dataset,
-#     label_field=["Ball", "Billiard table"],
-# )
-
-
 def pull_store_datasets(
     splits: list[str], pull_classes: list[str], export_classes: list[str]
 ) -> None:
+    """
+    Pulls and stores images from Google's open images api.
+
+    :param splits: Splits of images to pull. ["validation", "train", "test"]
+    :param pull_classes: List of class names that must be in the images.
+    :param export_classes: List of class names that are in the exported images. Any other labels in the images will be
+    ignored.
+
+    :Examples:
+    Pull all splits from open images with the class of Billiard Table
+    >>> pull_store_datasets(
+    >>>     ["validation", "train", "test"], ["Billiard table"], ["Ball", "Billiard table"]
+    >>> )
+    """
     for split in splits:
         print(f"Pulling {split} dataset...")
         dataset = fo.zoo.load_zoo_dataset(
@@ -34,7 +32,8 @@ def pull_store_datasets(
         )
         print(f"Computing {split} dataset metadata...")
         dataset.compute_metadata()
-        print(f"Evaluating {split} dataset detections...")
+        # Not sure if this is needed
+        # print(f"Evaluating {split} dataset detections...")
         # dataset.evaluate_detections(
         #     "predictions",
         #     gt_field="detections",
@@ -53,63 +52,17 @@ def pull_store_datasets(
 
 
 def load_dataset(dataset_dir: str, name: str):
+    """
+    Loads a dataset from disk. There are two ways to pull the dataset. If the dataset is in fo.list_datasets() then
+    specify the name and use fo.load_dataset(). Else, specify the path, name and use fo.Dataset.from_dir().
+
+    :param dataset_dir: Path to the dataset
+    :param name: Name of the dataset
+    """
     return fo.Dataset.from_dir(
         dataset_dir=dataset_dir, dataset_type=fo.types.YOLOv5Dataset, name=name
     )
+    # If the dataset is in fo.list_datasets(), then use this command to load dataset
     # return fo.load_dataset(name)
 
-
-pull_store_datasets(
-    ["validation", "train"], ["Billiard table"], ["Ball", "Billiard table"]
-)
-
-validation_data = load_dataset("datasets/snooker_vision/", "validation")
-validation_data = load_dataset("", name="v")
-
-#
-# test_dataset = fo.zoo.load_zoo_dataset(
-#     "open-images-v6",
-#     split="test",
-#     label_types=["detections"],
-#     classes=["Billiard table"],
-# )
-# print("dataset loaded. Exporting dataset...")
-# test_dataset.export(
-#     export_dir=os.path.join(os.path.dirname(__file__), "datasets/snooker_vision/test"),
-#     dataset_type=fo.types.YOLOv5Dataset,
-#     label_field=["Ball", "Billiard table"],
-# )
-#
-# validation_dataset = fo.zoo.load_zoo_dataset(
-#     "open-images-v6",
-#     split="validation",
-#     label_types=["detections"],
-#     classes=["Billiard table"],
-# )
-# print("dataset loaded. Exporting dataset...")
-# validation_dataset.compute_metadata()
-# validation_dataset.evaluate_detections(
-#     "predictions",
-#     gt_field="detections",
-#     method="open-images",
-#     classes=["Ball", "Billiard table"],
-#     use_boxes=True,
-# )
-# validation_dataset.export(
-#     # export_dir=os.path.join(
-#     #     os.path.dirname(__file__), "datasets/snooker_vision/validation"
-#     # ),
-#     export_dir="datasets/snooker_vision",
-#     dataset_type=fo.types.YOLOv5Dataset,
-#     # label_field=["Ball", "Billiard table"],
-#     split="validation",
-#     classes=["Ball", "Billiard table"],
-# )
-
-# train_results = train_dataset.evaluate_detections(
-#     "predictions",
-#     gt_field="detections",
-#     method="open-images",
-#     classes=["Ball", "Billiard table"],
-#     use_boxes=True,
-# )
+# validation_data = load_dataset("datasets/snooker_vision/", "validation")
