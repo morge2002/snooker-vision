8ed955de7a0e28bd8af6ffa09d6ddebdb79afb86
MorganLewis <sc20mcl@leeds.ac.uk>
2024-03-02 17:32:43 +0000

    pocket selection by user and run roi pot detection

--- yolov8/main.py
+++ yolov8/main.py
@@ -2,6 +2,8 @@ import cv2
 from ultralytics import YOLO
 from PIL import Image
 
+from yolov8.pot_detection import PocketROIHeuristic
+
 # Config
 image_inference = False
 video_inference = True
@@ -12,7 +14,7 @@ dataset_path = "dataset/snooker-vision.v2i.yolov8/data.yaml"
 model_path = "runs/detect/train13/weights/best_openvino_model"
 weights_path = "runs/detect/train13/weights/best.pt"
 
-video_path = "test_data/english_pool_video.mp4"
+video_path = "test_data/english_pool_dish_one_frame.mp4"
 
 # Train the model
 if train_model:
@@ -43,6 +45,47 @@ if video_inference:
     # Open the video file
     cap = cv2.VideoCapture(video_path)
 
+    # Store pocket coordinates
+    pocket_coordinates = []
+
+    def click_event(event, x, y, flags, param):
+        global pocket_coordinates
+        if event == cv2.EVENT_LBUTTONDOWN:
+            print(f"Clicked at ({x}, {y})")
+            pocket_coordinates.append([x, y])
+
+    # Check if the video file opened successfully
+    if not cap.isOpened():
+        print("Error: Unable to open video file")
+        exit()
+
+    # Read the first frame
+    ret, first_frame = cap.read()
+
+    # Check if the frame was read successfully
+    if not ret:
+        print("Error: Unable to read first frame from video")
+        exit()
+
+    # Create a window and display the first frame
+    cv2.namedWindow("Pocket Selection")
+    cv2.imshow("Pocket Selection", first_frame)
+
+    # Set the mouse callback function
+    cv2.setMouseCallback("Pocket Selection", click_event)
+
+    # Wait for the user to click six times
+    while len(pocket_coordinates) < 6:
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    # Release the window
+    cv2.destroyWindow("Pocket Selection")
+
+    rios = [5, 10, 20, 50]
+    pot_detector = PocketROIHeuristic(pocket_coordinates, rios)
+
     # Loop through the video frames
     while cap.isOpened():
         # Read a frame from the video
@@ -64,8 +107,16 @@ if video_inference:
             # Visualize the results on the frame
             annotated_frame = results[0].plot(labels=True, conf=False)
 
+            # Draw the pocket coordinates and RIOs on the frame
+            for pocket in pocket_coordinates:
+                for roi in rios:
+                    cv2.circle(annotated_frame, (pocket[0], pocket[1]), roi, (0, 0, 255), 2)
+
             # Display the annotated frame
             cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+            # Detect pot using ROI heuristic
+            pot_detector(results[0])
 
             # Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
