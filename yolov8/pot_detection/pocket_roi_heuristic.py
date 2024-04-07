9822061deda56628c8d012e8d21fdca4b4c4bb84
MorganLewis <sc20mcl@leeds.ac.uk>
2024-04-07 14:21:42 +0100

    roi detector returns balls potted but not in stored state

--- yolov8/pot_detection/pocket_roi_heuristic.py
+++ yolov8/pot_detection/pocket_roi_heuristic.py
@@ -12,9 +12,8 @@ class PocketROIHeuristic:
 
     The heuristic uses the following steps:
     1. Get the current state of the balls (which region of interests they are in).
-    2. Find the balls that are not in the current state but are in the previous state.
-    3. Check if the balls were in ROIs closer to the pockets (not the largest).
-    4. If the balls were in the closest ROIs, they are potted.
+    2. Check if the balls were in ROIs closer to the pockets (not the largest).
+    3. If the balls were in the closest ROIs and are missing, they are potted.
     """
 
     # Number of frames a ball must be missing before it's considered potted
@@ -22,7 +21,6 @@ class PocketROIHeuristic:
 
     def __init__(self, balls: Balls, pockets: Pockets):
         self.balls = balls
-        self.balls_potted: list[int] = []
         self.pockets = pockets.pockets
 
     def __call__(self, detection_results: ultralytics.engine.results.Results) -> list[int]:
@@ -35,13 +33,14 @@ class PocketROIHeuristic:
         if not isinstance(detection_results, ultralytics.engine.results.Results):
             raise ValueError("Detection results must be of type ultralytics.engine.results.Results.")
 
-        self.detect()
-        return self.balls_potted
+        balls_potted = self.detect()
+        return balls_potted
 
-    def detect(self):
+    def detect(self) -> list[int]:
         """
         Detects if a ball is potted.
         """
+        balls_potted: list[int] = []
         for ball_id, ball in self.balls.items():
             # Ball is potted if it is in a roi, is in the last ROI only, is missing for enough frames and not
             # already potted
@@ -49,7 +48,8 @@ class PocketROIHeuristic:
                 ball.pocket_roi != ()
                 and ball.pocket_roi[1] != [0]
                 and ball.missing_frame_count > self.missing_frame_threshold
-                and ball_id not in self.balls_potted
+                and not ball.pocketed
             ):
-                self.balls_potted.append(ball_id)
+                balls_potted.append(ball_id)
                 print(f"Ball {ball_id} potted in pocket {ball.pocket_roi[0]}")
+        return balls_potted
