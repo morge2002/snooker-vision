dc97edf41ad2b1f66bbc856c0865dff104a9496e
MorganLewis <sc20mcl@leeds.ac.uk>
2024-04-29 23:57:44 +0100

    Detect then project
    Tracking on original image then coords projected to the playing surface for pot detection

--- yolov8/cue_vision.py
+++ yolov8/cue_vision.py
@@ -16,10 +16,16 @@ from yolov8.user_input import UserInput
 class CueVision:
     # Relative to the current file
     tracker_file_path = os.path.join(os.path.dirname(__file__), "track/custom_bytetrack.yaml")
+    pocket_rois = [5, 25, 50]
+    table_dimensions = [320, 640]
 
-    def __init__(self, model_path: str):
+    def __init__(self, model_path: str, pocket_rois: list[int] = None, table_dimensions: list[int] = None):
         self.model_path = model_path
         self.model = YOLO(model_path)
+        if pocket_rois is not None:
+            self.pocket_rois = pocket_rois
+        if table_dimensions is not None:
+            self.table_dimensions = table_dimensions
 
     def __call__(self, video_path: str, show_video=True) -> dict[int, tuple]:
         # Open the video file
@@ -29,7 +35,7 @@ class CueVision:
 
         # Get and store the user-selected corners for table projection
         corner_coordinates = user_input.get_user_corners(cap, video_path)
-        table_projection = TableProjection(corner_coordinates)
+        table_projection = TableProjection(corner_coordinates, self.table_dimensions)
 
         # Project the first frame of the video to the table
         ret, first_frame = cap.read()
@@ -39,14 +45,13 @@ class CueVision:
         projected_frame = table_projection(first_frame)
 
         # Get and store the user-selected pockets for pocket detection
-        pocket_rois = [5, 25, 50]
         pocket_coordinates = user_input.get_user_pockets(projected_frame, video_path)
 
         # Class to track ball state
         balls = Balls()
 
         # Class to store the pockets and update which balls are in what pocket ROIs
-        pockets_tracker = Pockets(balls, pocket_coordinates, pocket_rois)
+        pockets_tracker = Pockets(balls, pocket_coordinates, self.pocket_rois)
 
         # Class to detect pots
         pot_detector = PotDetector(balls, pockets_tracker)
@@ -57,12 +62,6 @@ class CueVision:
             success, frame = cap.read()
 
             if success:
-                # Run YOLOv8 inference on the frame
-                # results = model.predict(frame, device="cpu", max_det=17)
-
-                # Project the frame to a 2:1 table
-                frame = table_projection(frame)
-
                 # Image detection and tracking
                 yolo_results = self.model.track(
                     frame,
@@ -77,6 +76,10 @@ class CueVision:
                 if detection_results.is_empty():
                     continue
 
+                # Project the frame to a 2:1 table
+                projected_frame = table_projection(frame)
+                table_projection.detection_projection(detection_results)
+
                 annotated_frame = frame
 
                 if show_video:
@@ -85,14 +88,14 @@ class CueVision:
 
                     # Draw the pocket coordinates and RIOs on the frame
                     for pocket in pocket_coordinates:
-                        for roi in pocket_rois:
-                            cv2.circle(annotated_frame, (pocket[0], pocket[1]), roi, (0, 0, 255), 2)
+                        for roi in self.pocket_rois:
+                            cv2.circle(projected_frame, (pocket[0], pocket[1]), roi, (0, 0, 255), 2)
 
                     # Draw the ball center points
                     for ball in detection_results:
                         cv2.circle(
-                            annotated_frame,
-                            (int(predicted_ball_coord[0]), int(predicted_ball_coord[1])),
+                            projected_frame,
+                            (int(ball["x"]), int(ball["y"])),
                             2,
                             (255, 0, 255),
                             2,
@@ -110,6 +113,7 @@ class CueVision:
                 if show_video:
                     # Display the annotated frame
                     cv2.imshow("YOLOv8 Inference", annotated_frame)
+                    cv2.imshow("Pot Detection", projected_frame)
 
                 # Break the loop if 'q' is pressed
                 if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -122,3 +126,19 @@ class CueVision:
         cap.release()
         cv2.destroyAllWindows()
         return pot_detector.successful_pots
+
+
+if __name__ == "__main__":
+    # Path to the YOLOv8 model
+    model_path = os.path.join(os.path.dirname(__file__), "./runs/detect/train14/weights/best_openvino_model")
+
+    # Path to the video file
+    # video_path = "../tests/test_data/english_pool_dish_one_frame.mp4"
+    # video_path = "../tests/test_data/snooker_clearance_portrait.mov"
+    # video_path = "../tests/test_data/9_ball_clearance.mp4"
+    # video_path = "../tests/test_data/english_pool_clearance_1.mp4"
+    video_path = "../tests/test_data/barton_snooker_clearance_10_pots.mp4"
+    # Run the CueVision pipeline
+    cue_vision = CueVision(model_path, pocket_rois=[5, 25, 50])
+    pots = cue_vision(video_path, show_video=True)
+    print(pots)
--- yolov8/table_segmentation/table_projection.py
+++ yolov8/table_segmentation/table_projection.py
@@ -2,15 +2,18 @@ from __future__ import annotations
 
 import cv2
 import numpy as np
+import ultralytics
+
+from yolov8.detection_results import DetectionResults
 
 
 class TableProjection:
-    def __init__(self, table_corners: list[list[int, int]]):
+    def __init__(self, table_corners: list[list[int, int]], table_dimensions: list[int]):
         self.table_corners = self.__reorder_corners(table_corners)
         # Define the destination points of the rectangle (4 corners)
         # Aspect ratio of 8-ball, 9-ball and snooker table is 2:1
-        self.dst_width = 320
-        self.dst_height = 640
+        self.dst_width = table_dimensions[0]
+        self.dst_height = table_dimensions[1]
         self.dst_points = np.array(
             [[0, 0], [self.dst_width, 0], [self.dst_width, self.dst_height], [0, self.dst_height]], dtype=np.float32
         )
@@ -22,6 +25,35 @@ class TableProjection:
     def table_projection(self, original_image):
         # Apply the perspective transformation
         return cv2.warpPerspective(original_image.copy(), self.projection_matrix, (self.dst_width, self.dst_height))
+
+    def bounding_box_projection(self, bounding_box: list[float]) -> tuple[float]:
+        x, y, w, h = bounding_box
+        # Create a list of the 4 corners of the bounding box
+        corners = np.array([[x, y], [x + w, y], [x + w, y + h], [x, y + h]], dtype=np.float32)
+        # Apply the perspective transformation to the bounding box
+        projected_corners = cv2.perspectiveTransform(corners.reshape(-1, 1, 2), self.projection_matrix)
+        # Get the bounding box of the projected corners
+        x, y, w, h = cv2.boundingRect(projected_corners)
+        return x, y, w, h
+        # # Calculate the bounding box of the projected corners
+        # min_x = np.min(projected_corners[:, :, 0])
+        # min_y = np.min(projected_corners[:, :, 1])
+        # max_x = np.max(projected_corners[:, :, 0])
+        # max_y = np.max(projected_corners[:, :, 1])
+        # new_x = min_x
+        # new_y = min_y
+        # new_w = max_x - min_x
+        # new_h = max_y - min_y
+        # return new_x, new_y, new_w, new_h
+
+    def detection_projection(self, detections: DetectionResults):
+        for ball in detections:
+            # Update the bounding box of the detection
+            new_x, new_y, new_w, new_h = self.bounding_box_projection([ball["x"], ball["y"], ball["w"], ball["h"]])
+            ball["x"] = new_x + new_w / 2
+            ball["y"] = new_y
+            ball["w"] = new_w
+            ball["h"] = new_h
 
     @staticmethod
     def __reorder_corners(table_corners: list[list[int, int]]) -> list[np.float32]:
